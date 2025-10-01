c33254faadef410f226256dde72400ac49f6ac21
qop1310 <madness191919@gmail.com>
2025-10-01 09:15:24 +0000

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.success("ㅇ")
-st.info("ㅇ")
-st.image("https://th.bing.com/th/id/OIP.Zw5UI6q9K1JEROnYT_MoVQHaEo?w=271&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")
+st.success("Mr. jon jones")
+st.info("27-1")
+st.image("https://cdn.spotvnews.co.kr/news/photo/201907/299822_370766_0442.jpg")
