3d728506820ca02d3428f15a380a8761eed0f361
qop1310 <madness191919@gmail.com>
2025-10-01 09:10:20 +0000

    첫 번째 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.success("ㅇ")
+st.info("ㅇ")
+st.image("https://th.bing.com/th/id/OIP.Zw5UI6q9K1JEROnYT_MoVQHaEo?w=271&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")
